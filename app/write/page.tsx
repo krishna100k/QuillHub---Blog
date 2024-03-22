71069ec012c3e6ea02056a53b944a9f9d29c7dc5
krishna100k <krishnaprasadawala@gmail.com>
2024-03-22 15:49:49 +0530

    changes

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -47,6 +47,39 @@ const Write = () => {
   const [imgAdd, setImgAdd] = useState<string | undefined>("");
   const [loading, setLoading] = useState<boolean>(false);
 
+  const modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+  ];
+
   return <div>Write</div>;
 };
 
